a0bef2ad4b2ddc719777d96491ae25f425de0947
alexey <alexey@cobr.info>
2025-03-13 18:42:06 +0200

    fix formatting

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,21 +17,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "storage_account" {
-  source              = "../../modules/storage_account"
+  source               = "../../modules/storage_account"
   storage_account_name = "devstorage${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
 }
 
 module "aks" {
-  count = var.deploy_aks ? 1 : 0
-  source = "../../modules/aks"
-  aks_name = "dev-aks"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  dns_prefix = "devaks"
+  count                  = var.deploy_aks ? 1 : 0
+  source                 = "../../modules/aks"
+  aks_name               = "dev-aks"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  dns_prefix             = "devaks"
   admin_group_object_ids = [var.admin_group_object_id]
-  tenant_id  = var.tenant_id
+  tenant_id              = var.tenant_id
 }
 
 resource "random_string" "suffix" {
